79ae2ba54130b3b33118bf2bb519fa5937e47f1f
RonKbS <stephen.bamwite@gmail.com>
2019-11-26 14:34:32 +0300

    popups in maps

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
-import MAPBTOKEN from 'env.js';
 
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+const mymap = L.map('mapid').setView([51.505, -0.09], 13);
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox.streets',
-    accessToken: MAPBTOKEN
+    accessToken: 'for_loop_'
 }).addTo(mymap);
+
+const marker = L.marker([51.5, -0.09]).addTo(mymap);
+const circle = L.circle([51.508, -0.11], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(mymap);
+const polygon = L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047]
+]).addTo(mymap);
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+circle.bindPopup("I am a circle.");
+polygon.bindPopup("I am a polygon.");
+var popup = L.popup()
+    .setLatLng([51.5, -0.09])
+    .setContent("I am a standalone popup.")
+    .openOn(mymap);
